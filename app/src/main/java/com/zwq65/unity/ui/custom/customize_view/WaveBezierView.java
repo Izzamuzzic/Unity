2a68dbeee00f99657e734f6d30428f7df2e6ed62
NlRVANA <651406441@qq.com>
2017-08-09 18:42:53 +0800

    demo

--- app/src/main/java/com/zwq65/unity/ui/custom/customize_view/WaveBezierView.java
+++ app/src/main/java/com/zwq65/unity/ui/custom/customize_view/WaveBezierView.java
@@ -23,13 +23,9 @@ public class WaveBezierView extends View {
 
     private Paint mPaint, mPaint2;
     private Path mPath, mPath2;
-    private int mWaveLength = 1000;
-    private int mOffset;
+    private float currPercent;
     private int mScreenHeight;
     private int mScreenWidth;
-    private int mWaveCount;
-    private int mCenterY;
-    private ValueAnimator animator;
 
     public WaveBezierView(Context context) {
         super(context);
@@ -41,8 +37,6 @@ public class WaveBezierView extends View {
 
     public WaveBezierView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mPath = new Path();
-        mPath2 = new Path();
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
 
@@ -57,22 +51,18 @@ public class WaveBezierView extends View {
     }
 
     private void startAnim() {
-        if (animator == null) {
-            animator = ValueAnimator.ofInt(0, mWaveLength);
-            animator.setDuration(2000);
-            animator.setRepeatCount(ValueAnimator.INFINITE);
-            animator.setInterpolator(new LinearInterpolator());
-            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    mOffset = (int) animation.getAnimatedValue();
-                    invalidate();
-                }
-            });
-            animator.start();
-        } else {
-            animator.resume();
-        }
+        ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
+        animator.setDuration(4000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                currPercent = (float) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        animator.start();
     }
 
     @Override
@@ -80,38 +70,69 @@ public class WaveBezierView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         mScreenHeight = h;
         mScreenWidth = w;
-        mWaveCount = (int) Math.round(mScreenWidth / mWaveLength + 1.5);
-        mCenterY = mScreenHeight / 2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mPath.reset();
-        mPath.moveTo(-mWaveLength + mOffset, mCenterY);
-        for (int i = 0; i < mWaveCount; i++) {
-            // + (i * mWaveLength)
-            // + mOffset
-            mPath.quadTo((-mWaveLength * 3 / 4) + (i * mWaveLength) + mOffset, mCenterY + 30, (-mWaveLength / 2) + (i * mWaveLength) + mOffset, mCenterY);
-            mPath.quadTo((-mWaveLength / 4) + (i * mWaveLength) + mOffset, mCenterY - 30, i * mWaveLength + mOffset, mCenterY);
-        }
-        mPath.lineTo(mScreenWidth, mScreenHeight);//移动到屏幕的两个底角
-        mPath.lineTo(0, mScreenHeight);
-        mPath.close();//回到初始点
+        mPath = getWavePath1(currPercent);
         canvas.drawPath(mPath, mPaint);
-
-
-        mPath2.reset();
-        mPath2.moveTo(-mWaveLength + mOffset, mCenterY);
-        for (int i = 0; i < mWaveCount; i++) {
-            // + (i * mWaveLength)
-            // + mOffset
-            mPath2.quadTo((-mWaveLength * 3 / 4) + (i * mWaveLength) + mOffset, mCenterY + 80, (-mWaveLength / 2) + (i * mWaveLength) + mOffset, mCenterY);
-            mPath2.quadTo((-mWaveLength / 4) + (i * mWaveLength) + mOffset, mCenterY - 80, i * mWaveLength + mOffset, mCenterY);
-        }
-        mPath2.lineTo(mScreenWidth, mScreenHeight);//移动到屏幕的两个底角
-        mPath2.lineTo(0, mScreenHeight);
-        mPath2.close();//回到初始点
+        mPath2 = getWavePath2(currPercent);
         canvas.drawPath(mPath2, mPaint2);
+
+    }
+
+    private Path getWavePath1(float percent) {
+        Path path = new Path();
+        int x = -mScreenWidth;
+        //当前x点坐标（根据动画进度水平推移，一个动画周期推移的距离为一个mWidth）
+        x += percent * mScreenWidth;
+        //设置起点
+        path.moveTo(x, mScreenHeight / 2);
+        //控制点的相对宽度
+        int quadWidth = mScreenWidth / 4;
+        //控制点的相对高度
+        int quadHeight = mScreenHeight / 20 * 3;
+        //相对起点绘制贝塞尔曲线（rQuadTo():相对path起点坐标绘制）
+        //第一个周期
+        path.rQuadTo(quadWidth, quadHeight, 2 * quadWidth, 0);
+        path.rQuadTo(quadWidth, -quadHeight, 2 * quadWidth, 0);
+        //第二个周期
+        path.rQuadTo(quadWidth, quadHeight, 2 * quadWidth, 0);
+        path.rQuadTo(quadWidth, -quadHeight, 2 * quadWidth, 0);
+        //右侧的直线
+        path.lineTo(x + mScreenWidth * 2, mScreenHeight);
+        //下边的直线
+        path.lineTo(x, mScreenHeight);
+        //自动闭合补出左边的直线
+        path.close();
+        return path;
+    }
+
+    private Path getWavePath2(float percent) {
+        Path path = new Path();
+        int x = -2 * mScreenWidth;
+        //当前x点坐标（根据动画进度水平推移，一个动画周期推移的距离为一个mWidth）
+        x += percent * 2 * mScreenWidth;
+        //设置起点
+        path.moveTo(x, mScreenHeight / 2);
+        //控制点的相对宽度
+        int quadWidth = mScreenWidth / 2;
+        //控制点的相对高度
+        int quadHeight = mScreenHeight / 5;
+        //相对起点绘制贝塞尔曲线（rQuadTo():相对path起点坐标绘制）
+        //第一个周期
+        path.rQuadTo(quadWidth, -quadHeight, 2 * quadWidth, 0);
+        path.rQuadTo(quadWidth, quadHeight, 2 * quadWidth, 0);
+        //第二个周期
+        path.rQuadTo(quadWidth, -quadHeight, 2 * quadWidth, 0);
+//        path.rQuadTo(quadWidth, quadHeight, 2 * quadWidth, 0);
+        //右侧的直线
+        path.lineTo(x + mScreenWidth * 3, mScreenHeight);
+        //下边的直线
+        path.lineTo(x, mScreenHeight);
+        //自动闭合补出左边的直线
+        path.close();
+        return path;
     }
 }
