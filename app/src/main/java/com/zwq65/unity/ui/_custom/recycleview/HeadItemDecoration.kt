b40a85cc92849903aa0593b9ebbcd48b5aa87bb1
Zhao Wenqiang <zwq651406441@gmail.com>
2018-05-17 15:28:21 +0800

    test

--- app/src/main/java/com/zwq65/unity/ui/_custom/recycleview/HeadItemDecoration.kt
+++ app/src/main/java/com/zwq65/unity/ui/_custom/recycleview/HeadItemDecoration.kt
@@ -42,7 +42,6 @@ class HeadItemDecoration : RecyclerView.ItemDecoration() {
     var mGroupInfoCallback: GroupInfoCallback? = null
     private var mHeaderHeight: Int = SizeUtils.dp2px(20f)
     private var mDividerHeight: Int = SizeUtils.dp2px(1f)
-    private var mPadding: Int = SizeUtils.dp2px(6f)
     private val mPaint by lazy {
         Paint().apply {
             this.color = ContextCompat.getColor(App.instance!!.applicationContext, R.color.chart_blue)
@@ -60,31 +59,54 @@ class HeadItemDecoration : RecyclerView.ItemDecoration() {
         val position = parent.getChildAdapterPosition(view)
         var groupInfo = mGroupInfoCallback?.getGroupInfo(position)
         //判断是否是同类别的第一个item
-        outRect.top = if (groupInfo?.position == 0) mHeaderHeight else mDividerHeight
+        outRect.top = if (true == groupInfo?.isFirstViewInGroup()) mHeaderHeight else mDividerHeight
     }
 
     override fun onDrawOver(c: Canvas?, parent: RecyclerView?, state: RecyclerView.State?) {
         super.onDrawOver(c, parent, state)
 
-        super.onDraw(c, parent, state)
         val childCount = parent?.childCount
         for (i in 0 until childCount!!) {
             val view = parent.getChildAt(i)
             val index = parent.getChildAdapterPosition(view)
             val groupInfo = mGroupInfoCallback?.getGroupInfo(index)
-            //只有组内的第一个ItemView之上才绘制
-            if (groupInfo?.position == 0) {
-                val left = parent.paddingLeft + mPadding
-                val top = view.top - (mHeaderHeight + mPadding)
-                val right = parent.width - (parent.paddingRight + mPadding)
-                val bottom = view.top + mPadding
-                c?.drawRect(left.toFloat() - mPadding, top.toFloat(), right.toFloat() + mPadding, bottom.toFloat(), mPaint)
-                //绘制Title
-                c?.drawText(groupInfo.content, left.toFloat(), bottom.toFloat(), mTextPaint)
+            val left = parent.paddingLeft
+            val right = parent.width - parent.paddingRight
+
+            //屏幕上第一个可见的 ItemView 时，i == 0;
+            if (i != 0) {
+                //只有组内的第一个ItemView之上才绘制
+                if (true == groupInfo?.isFirstViewInGroup()) {
+                    val top = view.top - mHeaderHeight
+                    val bottom = view.top
+                    drawHeaderRect(c, left, top, right, bottom, groupInfo)
+                }
+            } else {
+                //当 ItemView 是屏幕上第一个可见的View 时，不管它是不是组内第一个View
+                //它都需要绘制它对应的 StickyHeader。
+                var top = parent.paddingTop
+
+                //判断是否为一组items的最后一个item
+                if (true == groupInfo?.isLastViewInGroup()) {
+                    val suggestTop = view.bottom - mHeaderHeight
+                    //suggestTop<top，需要开始跟随item滑动
+                    if (suggestTop < top) {
+                        top = suggestTop
+                    }
+                }
+                val bottom = top + mHeaderHeight
+
+                drawHeaderRect(c, left, top, right, bottom, groupInfo)
             }
+
         }
     }
 
+    private fun drawHeaderRect(c: Canvas?, left: Int, top: Int, right: Int, bottom: Int, groupInfo: GroupInfo?) {
+        c?.drawRect(left.toFloat(), top.toFloat(), right.toFloat(), bottom.toFloat(), mPaint)
+        //绘制Title
+        c?.drawText(groupInfo?.content ?: "", left.toFloat(), bottom.toFloat(), mTextPaint)
+    }
 
     interface GroupInfoCallback {
         fun getGroupInfo(position: Int): GroupInfo
