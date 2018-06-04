8d833b617b962c7ee077e052bd7214fda857191e
Zhao Wenqiang <zwq651406441@gmail.com>
2018-06-04 16:08:57 +0800

    update Kotlin + buildSrc for Better Gradle Dependency Management

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,41 @@
+object Versions {
+    // Define versions in a single place
+
+    // Sdk and tools
+    val minSdkVersion = 19
+    val targetSdkVersion = 27
+    val compileSdkVersion = 27
+    val buildToolsVersion = "27.0.3"
+
+    // App dependencies
+    val junitVersion = "4.12"
+    val mockitoVersion = "2.11.0"
+    val kotlinVersion = "1.2.21"
+    val ankoVersion = "0.10.4"
+    val supportLibraryVersion = "27.1.1"
+    val constraintLayoutVersion = "1.1.0"
+    val okhttpVersion = "3.5.0"
+    val okhttpLoggingVersion = "3.4.1"
+    val retrofitVersion = "2.3.0"
+    val retrofitConverterVersion = "2.2.0"
+    val retrofitRxjava2AdapterVersion = "1.0.0"
+    val gsonVersion = "2.8.2"
+    val glideVersion = "4.6.1"
+    val greenDaoVersion = "3.2.0"
+    val greenDaoGeneratorVersion = "3.1.0"
+    val butterknifeVersion = "8.8.1"
+    val rxjavaVersion = "2.1.3"
+    val rxbindingVersion = "2.0.0"
+    val rxandroidVersion = "2.0.2"
+    val rxlifecycleVersion = "2.2.1"
+    val multidexVersion = "1.0.3"
+    val daggerVersion = "2.12"
+    val leakCanaryVersion = "1.5.4"
+    val stethoVersion = "1.5.0"
+    val buglyVersion = "2.6.6.1"
+    val easypermissionsVersion = "1.1.1"
+    val barlibraryVersion = "2.3.0"
+    val circleimageviewVersion = "2.2.0"
+    val photoViewVersion = "2.1.3"
+    val utilcodeVersion = "1.15.1"
+}
