27e7d1a32bbe563cd60493fde28d9387c7ebc4ce
zhaowenqiang <zwq651406441@gmail.com>
2020-01-16 18:30:43 +0800

    test kotlin coroutine

--- app/src/test/java/com/zwq65/unity/kotlin/Coroutine/7ExceptionHandling.kt
+++ app/src/test/java/com/zwq65/unity/kotlin/Coroutine/7ExceptionHandling.kt
@@ -2,9 +2,11 @@ package com.zwq65.unity.kotlin.Coroutine
 
 import kotlinx.coroutines.*
 import org.junit.Test
+import java.io.IOException
 
 /**
  * ================================================
+ * 异常处理
  * <p>
  * Created by NIRVANA on 2020/1/15.
  * Contact with <zwq651406441@gmail.com>
@@ -97,7 +99,7 @@ class `7ExceptionHandling` {
      *  这种行为不能被覆盖，且它被用来提供一个稳定的协程层次结构来进行结构化并发而无需依赖[CoroutineExceptionHandler]的实现。
      *  当父协程的所有子协程终止时，父协程将处理原始异常。
      *  这也是为什么，在这个例子中，[CoroutineExceptionHandler]总是被设置在由[GlobalScope]启动的协程中。
-     *  将异常处理者设置在[runBlocking]主作用域内启动的协程中是没有意义的，尽管子协程已经设置了异常处理者， 但是主协程也总是会被取消的。
+     *  将异常处理者设置在[runBlocking]主作用域内启动的协程中是没有意义的，因为主协程将始终在其子协程完成时被取消，尽管子协程设置了异常处理者.
      */
     @Test
     fun test4() = runBlocking<Unit> {
@@ -126,4 +128,154 @@ class `7ExceptionHandling` {
         }
         job.join()
     }
+
+    /**
+     *  异常聚合
+     *
+     *  如果协程的多个子协程抛出异常会怎样？
+     *  The general rule is "the first exception wins" 因此第一个引发的异常暴露给处理程序。
+     *  但这可能会导致丢失异常，例如，如果协程在其finally块中引发异常。因此会丢失其他异常。
+     *  解决方案之一是分别报告每个异常
+     *  但是[Deferred.await]应该具有相同的机制来避免不一致的行为，这将导致协程的实现细节（无论其是否将工作的一部分委派给其子代）泄漏给其异常处理程序
+     */
+    @Test
+    fun test5() = runBlocking<Unit> {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("Caught $exception with suppressed ${exception.suppressed.contentToString()}")
+        }
+        val job = GlobalScope.launch(handler) {
+            launch {
+                try {
+                    delay(Long.MAX_VALUE)
+                } finally {
+                    throw ArithmeticException()
+                }
+            }
+            launch {
+                delay(100)
+                throw IOException()
+            }
+            delay(Long.MAX_VALUE)
+        }
+        job.join()
+    }
+
+    /**
+     *  取消异常是透明的并且会在默认情况下解包
+     */
+    @Test
+    fun test6() = runBlocking<Unit> {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("Caught original $exception")
+        }
+        val job = GlobalScope.launch(handler) {
+            val inner = launch {
+                launch {
+                    launch {
+                        throw IOException()
+                    }
+                }
+            }
+            try {
+                inner.join()
+            } catch (e: CancellationException) {
+                //重新抛出CancellationException
+                println("Rethrowing CancellationException with original cause")
+                throw e
+            }
+        }
+        job.join()
+    }
+
+    /**
+     * 监督
+     *
+     * 正如我们之前研究的那样，取消是一种双向机制，在协程的整个层次结构之间传播。但是如果需要单向取消怎么办？
+     * 此类需求的一个良好示例是在其作用域内定义作业的 UI 组件。如果任何一个 UI 的子作业执行失败了，它并不总是有必要取消（有效地杀死）整个 UI 组件
+     * 但是如果 UI 组件被销毁了（并且它的作业也被取消了），那么就必须使所有子工作都失败，因为不再需要他们的结果
+     *
+     * 监督作业
+     * [SupervisorJob]可以被用于这些目的。它类似于常规的[Job]，唯一的不同是：[SupervisorJob]的取消只会向下传播。
+     */
+    @Test
+    fun test7() = runBlocking<Unit> {
+        val supervisor = SupervisorJob()
+        with(CoroutineScope(coroutineContext + supervisor)) {
+            // 启动第一个子作业——这个示例将会忽略它的异常（不要在实践中这么做！）
+            val firstChild = launch(CoroutineExceptionHandler { _, _ -> }) {
+                println("First child is failing")
+                throw AssertionError("First child is cancelled")
+            }
+            // 启动第两个子作业
+            val secondChild = launch {
+                firstChild.join()
+                // 取消了第一个子作业且没有传播给第二个子作业
+                println("First child is cancelled: ${firstChild.isCancelled}, but second one is still active")
+                try {
+                    delay(Long.MAX_VALUE)
+                } catch (e: Exception) {
+                    // 但是取消了监督的传播
+                    println("Exception:$e")
+                    println("Second child is cancelled because supervisor is cancelled")
+                }
+            }
+            // 等待直到第一个子作业失败且执行完成
+            firstChild.join()
+            println("Cancelling supervisor")
+            supervisor.cancel()
+            secondChild.join()
+        }
+    }
+
+    /**
+     * 监督作用域
+     *
+     * 对于作用域的并发[supervisorScope]可以被用来替代[coroutineScope]来实现相同的目的。
+     * 它只会单向的传播并且当作业自身执行失败的时候将所有子作业全部取消。
+     * 作业自身也会在所有的子作业执行结束前等待， 就像[coroutineScope]所做的那样。
+     */
+    @Test
+    fun test8() = runBlocking<Unit> {
+        try {
+            supervisorScope {
+                val child = launch {
+                    try {
+                        println("Child is sleeping")
+                        delay(Long.MAX_VALUE)
+                    } finally {
+                        println("Child is cancelled")
+                    }
+                }
+                // 使用 yield 来给我们的子作业一个机会来执行打印
+                yield()
+                println("Throwing exception from scope")
+                throw AssertionError()
+            }
+        } catch (e: AssertionError) {
+            println("Caught assertion error")
+        }
+    }
+
+    /**
+     * 监督协程中的异常
+     *
+     * 常规的作业和监督作业之间的另一个重要区别是异常处理。
+     * 监督协程中的每一个子作业应该通过异常处理机制处理自身的异常。
+     * 这种差异来自于子作业的执行失败不会传播给它的父作业。
+     */
+    @Test
+    fun test9() = runBlocking<Unit> {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("Caught $exception")
+        }
+        supervisorScope {
+            val child = launch(handler) {
+                println("Child throws an exception")
+                throw AssertionError()
+            }
+            println("Scope is completing")
+        }
+        println("Scope is completed")
+    }
+
 }
---
+++ app/src/test/java/com/zwq65/unity/kotlin/Coroutine/8SharedMutableStateAndConcurrency.kt
@@ -0,0 +1,193 @@
+package com.zwq65.unity.kotlin.Coroutine
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import org.junit.Test
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
+
+/**
+ * ================================================
+ * 共享的可变状态与并发
+ *
+ * 协程可用多线程调度器（比如默认的[Dispatchers.Default]）并发执行。它提出了所有常见的并发问题。
+ * 主要的问题是同步访问共享可变状态。
+ * 在协程领域中，此问题的某些解决方案与多线程世界中的解决方案相似，但其他解决方案则是唯一的。
+ * <p>
+ * Created by NIRVANA on 2020/1/15.
+ * Contact with <zwq651406441@gmail.com>
+ * ================================================
+ */
+class `8SharedMutableStateAndConcurrency` {
+
+    /**
+     * 问题
+     *
+     * 我们启动一百个协程，它们都做一千次相同的操作。
+     * 我们同时会测量它们的完成时间以便进一步的比较
+     */
+    private suspend fun massiveRun(action: suspend () -> Unit) {
+        val n = 100  // 启动的协程数量
+        val k = 1000 // 每个协程重复执行同一动作的次数
+        val time = measureTimeMillis {
+            coroutineScope {
+                // 协程的作用域
+                repeat(n) {
+                    launch {
+                        repeat(k) { action() }
+                    }
+                }
+            }
+        }
+        println("Completed ${n * k} actions in $time ms")
+    }
+
+    @Volatile // 在 Kotlin 中 `volatile` 是一个注解
+    private var counter = 0
+
+    /**
+     * 我们从一个非常简单的动作开始：使用多线程的 Dispatchers.Default 来递增一个共享的可变变量。
+     */
+    @Test
+    fun test1() = runBlocking<Unit> {
+        withContext(Dispatchers.Default) {
+            massiveRun {
+                counter++
+            }
+        }
+        /**
+         * 它不太可能打印出“Counter = 100000”，因为一百个协程在多个线程中同时递增计数器但没有做并发处理。
+         *
+         * volatile 无济于事
+         * 给[counter]加上[Volatile]后，这段代码运行速度更慢了，但我们最后仍然没有得到“Counter = 100000”这个结果，
+         * 因为[Volatile]变量保证可线性化（这是“原子”的技术术语）读取和写入变量，但在大量动作（在我们的示例中即“递增”操作）发生时并不提供原子性。
+         */
+        println("Counter = $counter")
+    }
+
+    /**
+     * 线程安全的数据结构
+     * 一种对线程、协程都有效的常规解决方法，就是使用线程安全（也称为同步的、 可线性化、原子）的数据结构，
+     * 它为需要在共享状态上执行的相应操作提供所有必需的同步处理。在简单的计数器场景中，
+     * 我们可以使用具有[AtomicInteger.incrementAndGet]原子操作的[AtomicInteger]类
+     * 这是针对此类特定问题的最快解决方案。它适用于普通计数器、集合、队列和其他标准数据结构以及它们的基本操作。
+     * 然而，它并不容易被扩展来应对复杂状态、或一些没有现成的线程安全实现的复杂操作。
+     */
+
+    /**
+     * 以细粒度限制线程
+     *
+     * 限制线程 是解决共享可变状态问题的一种方案：对特定共享状态的所有访问权都限制在单个线程中。
+     * 它通常应用于 UI 程序中：所有 UI 状态都局限于单个事件分发线程或应用主线程中。
+     * 这在协程中很容易实现，通过使用一个单线程上下文
+     */
+    @Test
+    fun test2() = runBlocking<Unit> {
+        val counterContext = newSingleThreadContext("CounterContext")
+
+        withContext(Dispatchers.Default) {
+            massiveRun {
+                // 将每次自增限制在单线程上下文中
+                withContext(counterContext) {
+                    counter++
+                }
+            }
+        }
+        /**
+         * 这段代码运行非常缓慢，因为它进行了 细粒度 的线程限制。
+         * 每个增量操作都得使用 [withContext(counterContext)] 块从多线程 Dispatchers.Default 上下文切换到单线程上下文
+         */
+        println("Counter = $counter")
+    }
+
+    /**
+     * 以细粒度限制线程
+     *
+     * 在实践中，线程限制是在大段代码中执行的，例如：状态更新类业务逻辑中大部分都是限于单线程中。
+     * 下面的示例演示了这种情况， 在单线程上下文中运行每个协程
+     */
+    @Test
+    fun test3() = runBlocking<Unit> {
+        val counterContext = newSingleThreadContext("CounterContext")
+
+        // 将一切都限制在单线程上下文中
+        withContext(counterContext) {
+            massiveRun {
+                counter++
+            }
+        }
+        /**
+         * 这段代码运行更快而且打印出了正确的结果。
+         */
+        println("Counter = $counter")
+    }
+
+    /**
+     * 互斥
+     *
+     * 该问题的互斥解决方案：使用永远不会同时执行的 关键代码块 来保护共享状态的所有修改。
+     * 在阻塞的世界中，你通常会为此目的使用[synchronized]或者 ReentrantLock。 在协程中的替代品叫做 Mutex 。
+     * 它具有 lock 和 unlock 方法， 可以隔离关键的部分。
+     * 关键的区别在于 Mutex.lock() 是一个挂起函数，它不会阻塞线程。
+     * 还有[withLock]扩展函数，可以方便的替代常用的 mutex.lock(); try { …… } finally { mutex.unlock() } 模式
+     */
+    @Test
+    fun test4() = runBlocking<Unit> {
+        val mutex = Mutex()
+        withContext(Dispatchers.Default) {
+            massiveRun {
+                // 用锁保护每次自增
+                mutex.withLock {
+                    counter++
+                }
+            }
+        }
+
+        /**
+         * 此示例中锁是细粒度的，因此会付出一些代价。
+         * 但是对于某些必须定期修改共享状态的场景，它是一个不错的选择，
+         * 但是没有自然线程可以限制此状态。
+         */
+        println("Counter = $counter")
+    }
+
+    /**
+     * Actors
+     *
+     * [actor]是由协程、该协程所限制和封装的状态 以及 与其他协程进行通信的通道的组合 组成的实体
+     *
+     */
+    // This function launches a new counter actor
+    fun CoroutineScope.counterActor() = actor<CounterMsg> {
+        var counter = 0 // actor state
+        for (msg in channel) { // iterate over incoming messages
+            when (msg) {
+                is IncCounter -> counter++
+                is GetCounter -> msg.response.complete(counter)
+            }
+        }
+    }
+
+    @Test
+    fun test5() = runBlocking<Unit> {
+        val counter = counterActor() // create the actor
+        withContext(Dispatchers.Default) {
+            massiveRun {
+                counter.send(IncCounter)
+            }
+        }
+        // send a message to get a counter value from an actor
+        val response = CompletableDeferred<Int>()
+        counter.send(GetCounter(response))
+        println("Counter = ${response.await()}")
+        counter.close() // shutdown the actor
+    }
+
+}
+
+// Message types for counterActor
+sealed class CounterMsg
+object IncCounter : CounterMsg() // one-way message to increment counter
+class GetCounter(val response: CompletableDeferred<Int>) : CounterMsg() // a request with reply
